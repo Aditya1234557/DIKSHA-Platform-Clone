2390848fe3442c745141718eb34f35c97bbeb4b5
Aditya1234557 <105175837+Aditya1234557@users.noreply.github.com>
2022-08-23 02:49:20 +0530

    Add files via upload

---
+++ project/diksha.js
@@ -0,0 +1,23 @@
+gsap.to("#nav a ,i",{
+    delay:0.8,
+    x:"100px",
+    ease: Expo.easeInout,
+    
+});
+
+
+
+gsap.to("#ashok ,#dya,#disksha",{
+    delay:0.8,
+    y: "85px",
+    ease: Expo.easeInout,
+    
+    
+});
+// gsap.to("#ashok ,#dya,#disksha,",{
+//     delay:.2,
+//     y: "85px",
+//     ease: Expo.easeInout,
+    
+    
+// });
